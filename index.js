73e7262d9e13a2f9d52f768e3eb36f573509e8c2
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-03-07 15:31:43 +0100

    Revert "[TECH] Déplacer le plugin eslint 1024pix directement dans ce projet"

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 const path = require('node:path');
-const noSinonStubWithArgsOneliner = require('./rules/no-sinon-stub-with-args-oneliner.js');
 
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -10,15 +8,7 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'plugin:i18n-json/recommended',
   ],
-  plugins: [
-    {
-      '@1024pix': {
-        rules: {
-          'no-sinon-stub-with-args-oneliner': noSinonStubWithArgsOneliner,
-        },
-      },
-    },
-  ],
+  plugins: ['@1024pix'],
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
