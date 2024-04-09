4aeb9bca4b3182925e9eb4aff6f3be845707e02b
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-04-09 15:53:06 +0200

    refactor: migrate to new eslint config file format

--- index.js
+++ index.js
@@ -1,110 +1,120 @@
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { resolve, dirname } from 'node:path';
+// import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import comments from '@eslint-community/eslint-plugin-eslint-comments/configs';
+import eslintPluginYml from 'eslint-plugin-yml';
+import pixPlugin from '@1024pix/eslint-plugin';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default {
-  extends: [
-    'eslint:recommended',
-    'plugin:yml/standard',
-    'plugin:@eslint-community/eslint-comments/recommended',
-    'plugin:i18n-json/recommended',
-  ],
-  plugins: ['@1024pix', 'simple-import-sort'],
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'script',
+export default [
+  js.configs.recommended,
+  ...eslintPluginYml.configs['flat/standard'],
+  comments.recommended,
+  // 'plugin:i18n-json/recommended' -- not ready
+  {
+    plugins: {
+      '@1024pix': pixPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'arrow-parens': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'computed-property-spacing': ['error', 'never'],
+      'eol-last': ['error'],
+      '@eslint-community/eslint-comments/no-unused-disable': ['error'],
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      'keyword-spacing': ['error'],
+      'linebreak-style': ['error', 'unix'],
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 1,
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            'NewExpression[callee.name=Date][arguments.length=1][arguments.0.type=Literal]:not([arguments.0.value=/^[12][0-9]{3}-(0[0-9]|1[0-2])-([0-2][0-9]|3[01])(T([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z)?$/])',
+          message: "Use only ISO8601 UTC syntax ('2019-03-12T01:02:03Z') in Date constructor",
+        },
+        {
+          selector:
+            "CallExpression[callee.object.object.name='faker'][callee.object.property.name='internet'][callee.property.name='email']",
+          message: 'Use only faker.internet.exampleEmail()',
+        },
+      ],
+      'no-restricted-globals': [
+        'error',
+        {
+          name: 'fetch',
+          message: "Use import fetch from 'fetch'",
+        },
+      ],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '_',
+        },
+      ],
+      'no-var': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'prefer-const': ['error'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'space-before-blocks': ['error'],
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'never',
+          named: 'never',
+          asyncArrow: 'ignore',
+        },
+      ],
+      'space-in-parens': ['error'],
+      'space-infix-ops': ['error'],
+      'func-call-spacing': ['error'],
+      'key-spacing': ['error'],
+      'comma-spacing': ['error'],
+      'no-trailing-spaces': ['error'],
+      'no-multi-spaces': ['error'],
+      'yml/quotes': [
+        'error',
+        {
+          prefer: 'single',
+          avoidEscape: true,
+        },
+      ],
+      //   'i18n-json/sorted-keys': [
+      //     'error',
+      //     {
+      //       sortFunctionPath: resolve(__dirname, './json/sort-translations.js'),
+      //     },
+      //   ],
+      '@1024pix/no-sinon-stub-with-args-oneliner': 'error',
+    },
   },
-  env: {
-    es6: true,
-    node: true,
-  },
-  rules: {
-    'arrow-parens': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'computed-property-spacing': ['error', 'never'],
-    'eol-last': ['error'],
-    '@eslint-community/eslint-comments/no-unused-disable': ['error'],
-    indent: [
-      'error',
-      2,
-      {
-        SwitchCase: 1,
-      },
-    ],
-    'keyword-spacing': ['error'],
-    'linebreak-style': ['error', 'unix'],
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1,
-        maxEOF: 1,
-      },
-    ],
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector:
-          'NewExpression[callee.name=Date][arguments.length=1][arguments.0.type=Literal]:not([arguments.0.value=/^[12][0-9]{3}-(0[0-9]|1[0-2])-([0-2][0-9]|3[01])(T([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z)?$/])',
-        message:
-          "Use only ISO8601 UTC syntax ('2019-03-12T01:02:03Z') in Date constructor",
-      },
-      {
-        selector:
-          "CallExpression[callee.object.object.name='faker'][callee.object.property.name='internet'][callee.property.name='email']",
-        message: 'Use only faker.internet.exampleEmail()',
-      },
-    ],
-    'no-restricted-globals': [
-      'error',
-      {
-        name: 'fetch',
-        message: "Use import fetch from 'fetch'",
-      },
-    ],
-    'no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '_',
-      },
-    ],
-    'no-var': ['error'],
-    'object-curly-spacing': ['error', 'always'],
-    'prefer-const': ['error'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'space-before-blocks': ['error'],
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'never',
-        named: 'never',
-        asyncArrow: 'ignore',
-      },
-    ],
-    'space-in-parens': ['error'],
-    'space-infix-ops': ['error'],
-    'func-call-spacing': ['error'],
-    'key-spacing': ['error'],
-    'comma-spacing': ['error'],
-    'no-trailing-spaces': ['error'],
-    'no-multi-spaces': ['error'],
-    'yml/quotes': [
-      'error',
-      {
-        prefer: 'single',
-        avoidEscape: true,
-      },
-    ],
-    'i18n-json/sorted-keys': [
-      'error',
-      {
-        sortFunctionPath: resolve(__dirname, './json/sort-translations.js'),
-      },
-    ],
-    '@1024pix/no-sinon-stub-with-args-oneliner': 'error',
-  },
-};
+];
+
+// export default {
+//     root: true,
+//     parserOptions: {
+//         ecmaVersion: 2018,
+//         sourceType: 'script',
+//     },
+//     env: {
+//         es6: true,
+//         node: true,
+//     },
+// };
